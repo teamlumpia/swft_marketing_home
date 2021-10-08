2980357e470bb1ae89bddb3ba3787f6c308c1bf8
Elaine Alasagas <ealasagas@gmail.com>
2021-10-07 19:49:40 -0700

    attempt to run in github pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,20 @@ const path = require('path')
 
 module.exports = {
   reactStrictMode: true,
+  images: { 
+    domains: ['i.ibb.co'],
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/menu": { page: "/menu" },
+      "/about": { page: "/about" }
+    };
+  }
 }
