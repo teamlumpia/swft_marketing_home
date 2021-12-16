2f91a5987ac1fdaeb3f2d163933a68186ee679d9
John Michael Laceda <john@johnlaceda.com>
2021-12-15 20:23:36 -0800

    initial ghpages deploy

---
+++ out/_next/static/chunks/pages/Menu-9793552cd9cebb678a10.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[213],{8143:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Menu",function(){return u(9334)}])}},function(n){n.O(0,[774,13,169,831,497,334,888,179],(function(){return _=8143,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
